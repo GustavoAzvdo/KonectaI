a1fb27a1afaba714e5e0ff40450004e971c34250
Gustavo Azevedo <ghca12@gmail.com>
2025-08-27 16:34:13 -0300

    feat(app): eliminando variaveis que não estão sendo usadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+
 import { ThemeProvider, CssBaseline } from "@mui/material";
-import { lightTheme, darkTheme } from "./theme";
+import { lightTheme } from "./theme";
 import Home from './pages/Home'
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+ 
 
   return (
     <ThemeProvider theme={lightTheme}>
